a6895529b1a503fe26580cf63dd090708c69bae6
nawelZerouali <132110383+nawelZerouali@users.noreply.github.com>
2025-02-08 23:49:31 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'auth_screen.dart';
+
+import 'firebase_options.dart'; // Pour les configurations Firebase
+
+void main() async {
+
+  runApp(const MyWidget());
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
